0fed3c800f43c4cf4a720fa3f993813c2ec075f4
DamirYang <damiryangirov24@gmail.com>
2026-04-07 15:55:18 +0300

    Task006 Test solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,24 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val button = findViewById<Button>(R.id.changeButton)
+        val textView = findViewById<TextView>(R.id.titleTextView)
+
+
+        button.setOnClickListener {
+            textView.text = "I am an Android Developer!"
+        }
     }
 }
